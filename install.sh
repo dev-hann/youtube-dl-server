3eb7cc041cbbf49e564146dddcb0b33fa19fab18
yoehwan <54878755+yoehwan@users.noreply.github.com>
2022-01-03 13:25:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ apt-get install -y systemctl
 # install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-
+sudo usermod -aG  docker $USER
 # install docker compose
 #curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #chmod +x /usr/local/bin/docker-compose
